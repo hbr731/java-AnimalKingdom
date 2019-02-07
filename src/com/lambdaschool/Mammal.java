824619bab94d24c00225cb963de7838cc97ef55b
habib1234731 <habibrehman1234731@gmail.com>
2019-02-06 23:30:54 -0500

    created the Mammal class

---
+++ src/com/lambdaschool/Mammal.java
@@ -0,0 +1,34 @@
+package com.lambdaschool;
+
+public class Mammal extends AbstractAnimal {
+    public String name;
+
+    public Mammal(String name, int year) {
+        super(year);
+        this.name = name;
+    }
+
+    @Override
+    public String move() {
+        return "walks";
+    }
+
+    @Override
+    public String breathe() {
+        return "breathes";
+    }
+
+    @Override
+    public String reproduce() {
+        return "live births";
+    }
+
+    @Override
+    public String toString() {
+        return "Mammal{" +
+                "name='" + name + '\'' +
+                ", food=" + food +
+                ", year=" + year +
+                '}';
+    }
+}
